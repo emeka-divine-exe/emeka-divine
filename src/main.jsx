6feb8334d92af874cbd67cfa31e06a8f569af52e
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-17 00:47:40 +0100

    Replace font imports with new font sources

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import '@fontsource/archivo-black'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/600.css'
-import '@fontsource/inter/700.css'
+import '@fontsource-variable/onest'
+import '@fontsource/public-sans/400.css'
+import '@fontsource/public-sans/500.css'
+import '@fontsource/public-sans/600.css'
+import '@fontsource/public-sans/700.css'
+import '@fontsource/public-sans/900.css'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
