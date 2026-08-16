fc551bc915f56cbc16768de402bc48d0d7901a25
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-16 17:17:57 +0100

    Add font imports for Archivo Black and Inter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '@fontsource/archivo-black'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
