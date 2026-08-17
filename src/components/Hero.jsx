2b5c3cbf8edd2e6cd19d6ea893a8c441c219a30d
Emeka Divine <emekadivine.exe@gmail.com>
2026-08-17 02:14:33 +0100

    Adjust Hero component heading styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -139,8 +139,8 @@ export default function Hero() {
             <h1
               className="
                 font-display
-                max-w-[580px]
-                text-[2rem]
+                max-w-[460px]
+                text-[2.8rem]
                 font-medium
                 leading-[0.96]
                 tracking-[-0.055em]
